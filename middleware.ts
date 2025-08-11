843b0e6d9246558b40c610a6516d2a68e3615669
Muhammad Ibad <muhammadibad766@gmail.com>
2025-08-11 22:21:15 +0500

    fix redirect bugs

--- middleware.ts
+++ middleware.ts
@@ -35,7 +35,9 @@ export function middleware(req: NextRequest) {
   if (isAuth && isGuestRoute && pathname !== "/home") {
     return NextResponse.redirect(new URL("/home", req.url));
   }
-
+  if (!isAuth && pathname === "/home") {
+    return NextResponse.redirect(new URL("/auth/signin", req.url));
+  }
   return NextResponse.next();
 }
 
